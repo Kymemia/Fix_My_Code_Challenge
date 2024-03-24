79826f110d1a454dc284d0bb04fb47d1bad4a67b
Kymemia <kimemiachristopherr@gmail.com>
2024-03-25 01:39:07 +0300

    3.0

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -37,7 +37,7 @@ class User():
         - `None` if `pwd` is not a string
         - Hash `pwd` in MD5 before assign to `__password`
         """
-        if pwd is None or type(pwd) is not str:
+        if not isinstance(pwd, str):
             self.__password = None
         else:
             self._password = hashlib.md5(pwd.encode()).hexdigest()
@@ -50,11 +50,11 @@ class User():
         - `False` if `__password` is `None`
         - Compare `__password` and the MD5 value of `pwd`
         """
-        if pwd is None or type(pwd) is not str:
+        if not isinstance(pwd, str):
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return self.__password == hashlib.md5(pwd.encode()).hexdigest()
 
 
 if __name__ == '__main__':
